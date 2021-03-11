1d555e532c26e9d0d6408f779e0aede28024f929
Gustavo <gustavoapereirau@gmail.com>
2021-03-10 23:21:19 -0500

    actualizado

--- src/js/app.js
+++ src/js/app.js
@@ -16,12 +16,29 @@ async function mostrarServicios(){
 
         //Generar HTML
         servicios.forEach(servicio => {
-
+            const { id, nombre, precio } = servicio;
             //DOM Scripting
+            //Generar Nombre
             const nombreServicio = document.createElement('P');
-            nombreServicio.textContent = servicio.nombre;
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
 
-            console.log(nombreServicio);
+            //Generar Precio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            //Generar Div contenedor de servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+
+            //inyectar precio y nombre al div del servicio
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            //Inyectarlo en HTML
+            document.querySelector('#servicios').appendChild(servicioDiv);
         })
     } catch (error) {
         console.log(error);
